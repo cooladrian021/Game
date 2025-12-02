98495040b803a59712bc318ab34d64becc8ad104
Adrian Madrigal <cooladrian021@gmail.com>
2025-12-01 16:07:35 -0800

    I coded half of the ground movement.

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -15,3 +15,19 @@ func _unhandled_input(event):
 		
 	elif event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
+
+func _physics_process(_delta):
+	const SPEED = 5.5 
+	
+	var input_direction_2D = Input.get_vector(
+	"move_left", "move_right", "move_foward", "move_back"
+	)
+	var input_direction_3D = Vector3(
+		input_direction_2D.x, 0.0, input_direction_2D.y
+	)
+	var direction = transform.basis * input_direction_3D
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	move_and_slide()
